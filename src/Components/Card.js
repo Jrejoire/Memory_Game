ed90441797ca4a8be31d2ad754870de69c92ff2c
Jrej <jrejoire@hotmail.fr>
2019-04-14 01:44:53 +0200

    Updating files

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './Card.css';
 
-const Card = ({ letterProp, faceProp, selectProp}) => {
-	return(
-		letterProp.map((letter,i,j) =>{
-			return(
-				<img id={`${selectProp.includes(letter) & (faceProp)?'face':''}`} 
-				className={letter} key={i} src={require('./399772.jpg')} alt='Silhouette'
-				/>
-			)
-		})
-	);
+const Card = ({ letterProp, faceProp, selectProp, foundProp }) => {
+  return(
+    letterProp.map((letter,i,j) =>{
+      return(
+        <img id={`${(selectProp.includes(letter) && faceProp)||foundProp.includes(letter)?'face':''}`} 
+        className={letter} key={i} src={require('./399772.jpg')} alt='Silhouette'
+        />
+      )
+    })
+  );
 }
 
 export default Card;
-//${selectProp==={letter} & faceProp?'face':''}
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -3,90 +3,127 @@ import './App.css';
 import Card from '../Components/Card';
 
 class App extends Component {
-	constructor(){
-		super()
-		this.state = {
-			listLetters:[],
-			select:[],
-			found:[],
-			face:false,
-			clicked:0
-		}
-	}
+  constructor(){
+    super()
+    this.state = {
+      listLetters:[],
+      select:[],
+      found:[],
+      face:false,
+      clicked:0
+    }
+  }
+  //In order to add the clicked image to the selection state
+  imageSelection = (event) => {
+    let selected= this.state.select.concat(event.target.className);
+    this.setState({ select: selected });
+    console.log(event.target.className);
+  }
 
-	imageSelection = (event) => {
-		let selected= this.state.select.concat(event.target.className);
-		this.setState({ select: selected });
-		console.log(event.target.className);
-	}
+  //Timeout function closing the turn cycle
+  timeout =(time)=> {
+    let delay;
+    function startTimeout(time){
+      let click = this.state.clicked;
+      delay = setTimeout(function(){ click =+ 1 }, time);
+      this.setState({ clicked: click });
+    }
 
-	handleClick = (event) => {
-		this.setState({ clicked: this.state.clicked +1 })
+    function stopTimeout(){clearTimeout(delay);
+    }
+  }
+  
+  //Conditions when image clicked
+  handleClick = (event) => {
+    this.setState({ clicked: this.state.clicked +1 });
 
-		if (this.state.clicked <1 ){
-		this.imageSelection(event);
-		this.setState({ face: !this.state.face });
-		}
+    // Do nothing when background is clicked
+    if(event.target.className === 'container'){
+      this.setState({ clicked: this.state.clicked });
+    }
 
-		const targetChar = event.target.className.charAt(0);
-		const selectChar = this.state.select.map(letter => letter.charAt(0));
+    // Rules of the game:
+    // At first click
 
-		if (this.state.clicked >=1 && this.state.select.includes(event.target.className)){
-		this.setState({ clicked: this.state.clicked })
-		}		
-			else if (this.state.clicked >= 1 && selectChar.includes(targetChar)){
-				console.log('SAME');
-				this.imageSelection(event);
-				this.setState({ face: true });
-			}					
-			else if (this.state.clicked >= 1 && !selectChar.includes(targetChar)){
-				console.log('NOT SAME');
-				this.imageSelection(event);
-				this.setState({ face: true });
-			}
-		if(this.state.clicked === 2){
-			this.setState({ clicked: 0});
-			this.setState({ face: !this.state.face });
-			this.setState({ select: [] });
-		}
-	}
+    if (this.state.clicked <1 ){
+      this.imageSelection(event);
+      this.setState({ face: !this.state.face });
+    }
 
-	componentWillMount(){
-	//Preparing the list of letters (Shuffle + Random)
-		function shuffle(arr){
-	  		let i,j,x;
-	  		for (i =arr.length-1; i>0; i--){
-	  			j = Math.floor(Math.random()*(i+1));
-	  			x =arr[i];
-	  			arr[i] = arr[j];
-	  			arr[j] = x;
-	  		}
-  			return arr;
-  		}
-		const letters = ['A','A2','B','B2','C','C2','D','D2','E','E2','F','F2','G','G2','H','H2','I','I2','J','J2','K','K2'];
-	  	const i = Math.floor(Math.random()*11);
-	  	const oddLetters = letters.slice(0,i).concat(letters.slice(i+1, letters.length));
-  		const randomOddLetters = shuffle(oddLetters);
-  	
-  		this.setState({ listLetters:randomOddLetters });
-  	}
-  	////////////////////////////////////////////////////////
+    // At second click
 
-	render() {
-		const { listLetters, face, select } = this.state;
-		return (
-		    <div className = 'body'>
-		      <div className="App">
-		        <h1>PI</h1>
-		        <h2 >Puzzle Investigator</h2>
-		        <br />
-		        <div className='container' onClick={this.handleClick}>
-		        <Card letterProp={listLetters} faceProp={face} selectProp={select}/> 
-		        </div>
-		      </div>
-		    </div>
-	    );
-	}
+    const targetChar = event.target.className.charAt(0);
+    const selectChar = this.state.select.map(letter => letter.charAt(0));
+    
+    if (this.state.clicked >=1 && this.state.select.includes(event.target.className)){
+      this.setState({ clicked: this.state.clicked })
+    }   
+      else if (this.state.clicked >= 1 && selectChar.includes(targetChar)){
+        console.log('SAME');
+        this.imageSelection(event);
+        this.setState({ face: true });
+        let pairFound= this.state.found.concat(this.state.select,event.target.className);
+        this.setState({ found: pairFound });
+
+        // timeout condition that closes the turn (cf next step)
+        this.timeout(500)
+      }         
+      else if (this.state.clicked >= 1 && !selectChar.includes(targetChar)){
+        console.log('NOT SAME');
+        this.imageSelection(event);
+        this.setState({ face: true });
+        
+        // timeout condition that closes the turn (cf next step)
+        this.timeout(3000);
+      }
+
+    // At 3rd click - To close turn cycle
+
+    if(this.state.clicked === 2){
+
+      this.setState({ clicked: 0});
+      this.setState({ face: !this.state.face });
+      this.setState({ select: [] });
+    }
+  }
+
+
+  componentWillMount(){
+  //Preparing the list of letters (Shuffle + Random)
+    function shuffle(arr){
+        let i,j,x;
+        for (i =arr.length-1; i>0; i--){
+          j = Math.floor(Math.random()*(i+1));
+          x =arr[i];
+          arr[i] = arr[j];
+          arr[j] = x;
+        }
+        return arr;
+      }
+    const letters = ['A','A2','B','B2','C','C2','D','D2','E','E2','F','F2','G','G2','H','H2','I','I2','J','J2','K','K2'];
+      const i = Math.floor(Math.random()*11);
+      const oddLetters = letters.slice(0,i).concat(letters.slice(i+1, letters.length));
+      const randomOddLetters = shuffle(oddLetters);
+    
+      this.setState({ listLetters:randomOddLetters });
+    }
+    ////////////////////////////////////////////////////////
+
+  render() {
+    const { listLetters, face, select, found } = this.state;
+    return (
+        <div className = 'body'>
+          <div className="App">
+            <h1>PI</h1>
+            <h2 >Puzzle Investigator</h2>
+            <br />
+            <div className='wrapp' onClick={this.handleClick}>
+            <Card letterProp={listLetters} faceProp={face} selectProp={select} foundProp={found}/> 
+            </div>
+          </div>
+        </div>
+      )
+  }
 
 }
 
@@ -95,25 +132,21 @@ export default App;
 
 /*at render page:
 delete random card --- DONE
-
 when first card clicked: ---DONE
-	-change image --- DONE
+  -change image --- DONE
 when second card clicked 
-	- change image
-
+  - change image
 if className first card = className second card then:
 -border green
 - msg (validation)
-
 otherwise 
 timer 3s then change both image back
-
 when only K left + k clicked
 - win message*/
 
 /* 
-	1/click on 1 image
-	2/click on 2nd image
-	3/ if image1 === image2 then set id to face permanently
-	-otherwise timer 3 s to face false all + select ''
+  1/click on 1 image
+  2/click on 2nd image
+  3/ if image1 === image2 then set id to face permanently
+  -otherwise timer 3 s to face false all + select ''
 */
