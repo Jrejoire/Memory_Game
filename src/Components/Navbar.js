d0f77cfc1e691a7586b027babd7c1e475a3257cd
Jrej <jrejoire@hotmail.fr>
2019-04-23 06:18:55 +0200

    removed svg arrow from master branch

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,9 +6,6 @@ const Nav = () =>{
 		<div className='navcontainer'>
 			<h1 className='item'>MEMORY GAME: </h1>
 			<h2 className='item'>Investigation Edition</h2>
-			<svg className='svg' width="90" height="90">
-				<img src="./arrows.svg" alt="./arrowimage.jpg" width="90" height="90" />
-			</svg>
 		</div>
 	)
 }
